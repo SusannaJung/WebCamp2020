571d977040ebb1c27c0e7ff34e0763d0b79f268b
SusannaJung <21800662@handong.edu>
2020-01-21 14:02:56 +0900

    spring4

---
+++ Spring4/README.md
@@ -0,0 +1,3 @@
+<img src="home.png" width="80%">
+<br>
+<img src="regist.png" width="80%">
---
+++ Spring4/src/main/java/com/handong/myapp/BoardService.java
@@ -0,0 +1,25 @@
+package com.handong.myapp;
+
+import java.util.List;
+
+public interface BoardService {
+	
+	  public void regist(BoardVO board) throws Exception;
+
+
+
+	  public BoardVO read(Integer bno) throws Exception;
+
+
+
+	  public void modify(BoardVO board) throws Exception;
+
+
+
+	  public void remove(Integer bno) throws Exception;
+
+
+
+	  public List<BoardVO> listAll() throws Exception;
+
+}
